f8afe957ebfe03d4a5998df6b94f70e07a888e5b
Marc Fargas <marc@fargas.com>
2014-09-13 16:35:05 +0200

    Missing imports

--- importasol/db/gestorsol/__init__.py
+++ importasol/db/gestorsol/__init__.py
@@ -1,4 +1,6 @@
 from cli import CLI
-from otros import FPA
+from otros import FPA, FAM, AGE
+from ser import SER
+from facturas import FAC, LFA, LCO
 
-__all__ = [CLI, FPA]
+__all__ = [CLI, FPA, FAM, SER, FAC, LFA, LCO, AGE]
