6f754736f5ee1d9a51ec64f91dc626bb2d1f04ec
Marc Fargas <marc@fargas.com>
2014-09-13 16:33:58 +0200

    Missing imports

--- importasol/db/contasol/__init__.py
+++ importasol/db/contasol/__init__.py
@@ -1,5 +1,6 @@
-from .otros import MAE
+from .otros import MAE, DEP, SDE
 from .apu import APU
 from .asi import Asiento
+from .iva import IVS, IVR
 
-__all__ = [APU, MAE, Asiento]
+__all__ = [APU, MAE, Asiento, DEP, SDE, IVS, IVR]
