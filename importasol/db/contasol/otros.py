005a88fc1ade844b1584daa4994345b8380f72f4
Marc Fargas <marc@fargas.com>
2014-10-18 14:36:32 +0200

    archivo PRO

--- importasol/db/contasol/otros.py
+++ importasol/db/contasol/otros.py
@@ -1,5 +1,5 @@
 from ..base import SOLFile
-from ..fields import CampoA, CampoT, CampoN, CampoCuenta
+from ..fields import CampoA, CampoT, CampoN, CampoCuenta, CampoND
 
 
 class MAE(SOLFile):
@@ -54,6 +54,55 @@ class SDE(SOLFile):
 
     def __unicode__(self):
         t = u"SDE(%s.%s: %s)" % (self.cA, self.cB, self.cC)
+        return t
+
+    __str__ = __unicode__
+    __repr__ = __unicode__
+
+
+class PRO(SOLFile):
+    cA = CampoN("Codigo", size=5, required=True)
+    cB = CampoA("Nombre", size=100)
+    cC = CampoA("NIF", truncate=False, size=12)
+    cD = CampoA("Sigla del Domicilio", size=2)
+    cE = CampoA("Domicilio", size=100)
+    cF = CampoA("Numero de la calle", size=6)
+    cG = CampoN("Codigo Postal", size=5)
+    cH = CampoA("Poblacion", size=30)
+    cI = CampoA("Provincia", size=20)
+    cJ = CampoA("Telefono", truncate=False, size=15)
+    cK = CampoA("Fax", size=15, truncate=False)
+    cL = CampoA("Movil", size=15, truncate=False)
+    cM = CampoA("Concepto Debe", size=40)
+    cN = CampoN("Tipo de operaciones", size=1)
+    cO = CampoA("Pais", size=3)
+    cP = CampoA("Persona de Contacto", size=50)
+    cQ = CampoA("Email", size=50)
+    cR = CampoND("Porcen IVA", size=5)
+    cR = CampoND("Porcen Recargo", size=5)
+    cT = CampoCuenta("Cuenta Contrapartida 1", size=10)
+    cU = CampoCuenta("Cuenta Contrapartida 2", size=10)
+    cV = CampoCuenta("Cuenta Contrapartida 3", size=10)
+    cW = CampoA("Banco", size=50, truncate=False)
+    cX = CampoA("Entidad", size=4, truncate=False)
+    cY = CampoA("Oficina", size=4, truncate=False)
+    cZ = CampoA("DC", size=2, truncate=False)
+    cAA = CampoA("Cuenta", size=10, truncate=False)
+    cAB = CampoA("Mensaje Emergente", size=50)
+    cAF = CampoN("Tipo de retenciones", size=1)
+    cAG = CampoND("Porcen Retencion", size=5)
+    cAH = CampoN("Tipo de deduccion", size=1)
+    cAI = CampoA("IBAN", size=30)
+    cAK = CampoN("Identificacion Fiscal", size=1)
+    cAL = CampoN("Tipo de Impuesto", size=1)
+    cAM = CampoN("Clave de operacion habitual", size=2)
+    cAO = CampoN("Tipo de IVA predefinido", size=1)
+
+    class Meta:
+        tabla = 'PRO'
+
+    def __unicode__(self):
+        t = u"PRO(%s, %s)" % (self.cA, self.cB)
         return t
 
     __str__ = __unicode__
